0252726090241b88ae25d6f3659e3ece1197fb27
Nkwi Cyril <akinimbomnkwi@gmail.com>
2026-05-16 23:14:59 +0100

    feat: add FAQ section to home page and include data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -6,31 +6,41 @@ import { Testimonials } from "@/components/sections/testimonials";
 import { Blog } from "@/components/sections/blog";
 import { CTA } from "@/components/sections/cta";
 import { TrustBar } from "@/components/sections/trust-bar";
+import { Faq } from "@/components/sections/faq";
 import {
   getServices,
   getProjects,
   getFeedbacks,
   getBlogPosts,
   getTechStack,
+  getFaqs,
 } from "@/utils/supabase/queries";
 
 export const revalidate = 60;
 
 export default async function Home() {
-  const [servicesRes, projectsRes, feedbacksRes, blogRes, techRes] =
-    await Promise.all([
-      getServices(50, 0),
-      getProjects(50, 0),
-      getFeedbacks(50, 0),
-      getBlogPosts(6, 0, true),
-      getTechStack(),
-    ]);
+  const [
+    servicesRes,
+    projectsRes,
+    feedbacksRes,
+    blogRes,
+    techRes,
+    faqsRes,
+  ] = await Promise.all([
+    getServices(50, 0),
+    getProjects(50, 0),
+    getFeedbacks(50, 0),
+    getBlogPosts(6, 0, true),
+    getTechStack(),
+    getFaqs(true),
+  ]);
 
   const services = servicesRes.data ?? [];
   const projects = projectsRes.data ?? [];
   const feedbacks = feedbacksRes.data ?? [];
   const blogPosts = blogRes.data ?? [];
-  // const techStack = techRes.data ?? [];
+  const techStack = techRes.data ?? [];
+  const faqs = faqsRes.data ?? [];
 
   return (
     <>
@@ -40,7 +50,8 @@ export default async function Home() {
       <PortfolioPreview projects={projects} />
       <Testimonials feedbacks={feedbacks} />
       <Blog posts={blogPosts} />
-      {/* <TrustBar techStack={techStack} /> */}
+      <Faq faqs={faqs} />
+      <TrustBar techStack={techStack} />
       <CTA />
     </>
   );
